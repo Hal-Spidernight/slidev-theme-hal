5b0255eeb9fa4f0729a2293b6d996c6a0659a861
Hal <hal.spidernight@gmail.com>
2025-11-14 15:32:52 +0900

    chore: remove debug lines

--- layouts/cover.vue
+++ layouts/cover.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="slidev-layout cover">
-    <p>hoge</p>
     <div class="my-auto w-full">
       <slot />
     </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="slidev-layout default">
-    <p>hoge</p>
     <div class="my-auto w-full">
       <slot />
     </div>
